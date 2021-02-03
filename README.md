370cb417b241178878aa162db051830ef3b1880f
Jared Johnson <jared@jaredjohnson.dev>
2021-02-02 21:58:13 -0500

    Remove TS enums from command

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ notification service
 [![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
 [![Version](https://img.shields.io/npm/v/revere.svg)](https://npmjs.org/package/revere)
 [![Downloads/week](https://img.shields.io/npm/dw/revere.svg)](https://npmjs.org/package/revere)
-[![License](https://img.shields.io/npm/l/revere.svg)](https://github.com/jaredjj3/revere/blob/master/package.json)
+[![License](https://img.shields.io/npm/l/revere.svg)](https://github.com/git@github.com:jaredjj3/revere.git/blob/master/package.json)
 
 <!-- toc -->
 * [Usage](#usage)
@@ -19,7 +19,7 @@ $ npm install -g revere
 $ revere COMMAND
 running command...
 $ revere (-v|--version|version)
-revere/1.0.0 darwin-x64 node-v14.3.0
+revere/1.0.0 darwin-x64 node-v14.15.4
 $ revere --help [COMMAND]
 USAGE
   $ revere COMMAND
@@ -29,6 +29,7 @@ USAGE
 # Commands
 <!-- commands -->
 * [`revere help [COMMAND]`](#revere-help-command)
+* [`revere notify`](#revere-notify)
 
 ## `revere help [COMMAND]`
 
@@ -46,4 +47,20 @@ OPTIONS
 ```
 
 _See code: [@oclif/plugin-help](https://github.com/oclif/plugin-help/blob/v3.2.1/src/commands/help.ts)_
+
+## `revere notify`
+
+runs specified detectors and notifiers
+
+```
+USAGE
+  $ revere notify
+
+OPTIONS
+  -d, --detectors=detectors  [default: squoze]
+  -h, --help                 show CLI help
+  -n, --notifiers=notifiers  [default: console]
+```
+
+_See code: [src/commands/notify.ts](https://github.com/jaredjj3/revere/blob/v1.0.0/src/commands/notify.ts)_
 <!-- commandsstop -->
--- src/commands/notify.ts
+++ src/commands/notify.ts
@@ -1,16 +1,16 @@
 import { Command, flags } from '@oclif/command';
-import { sortBy } from '@oclif/plugin-help/lib/util';
-import { Detector, DetectorName } from '../detectors';
+import { flatten, sortBy } from 'lodash';
+import { Detector } from '../detectors';
 import { RevereError } from '../errors';
 import { container } from '../inversify.config';
 import { Message } from '../messages';
-import { Notifier, NotifierName } from '../notifiers';
+import { Notifier } from '../notifiers';
 import { capitalize } from '../util';
 
-const ALLOWED_DETECTORS = [DetectorName.Squoze];
-const ALLOWED_NOTIFIERS = [NotifierName.Console, NotifierName.Discord];
-const DEFAULT_DETECTORS = [DetectorName.Squoze];
-const DEFAULT_NOTIFIERS = [NotifierName.Console];
+const ALLOWED_DETECTORS = ['squoze'];
+const ALLOWED_NOTIFIERS = ['console', 'discord'];
+const DEFAULT_DETECTORS = ['squoze'];
+const DEFAULT_NOTIFIERS = ['console'];
 
 export default class Notify extends Command {
   static description = 'runs specified detectors and notifiers';
@@ -36,7 +36,7 @@ export default class Notify extends Command {
 
   private async getMessages(detectors: Detector[]): Promise<Message[]> {
     const result = await Promise.all(detectors.map((detector) => detector.detect()));
-    const messages = result.flat();
+    const messages = flatten(result);
     return sortBy(messages, (message) => message.detectedAt.getMilliseconds());
   }
 
@@ -51,7 +51,7 @@ export default class Notify extends Command {
   }
 
   private getDetector(detectorName: string): Detector {
-    if (!(ALLOWED_DETECTORS as string[]).includes(detectorName)) {
+    if (!ALLOWED_DETECTORS.includes(detectorName)) {
       throw new RevereError(`detector not allowed: ${detectorName}`);
     }
     const serviceIdentifier = `${capitalize(detectorName)}Detector`;
@@ -59,7 +59,7 @@ export default class Notify extends Command {
   }
 
   private getNotifier(notifierName: string): Notifier {
-    if (!(ALLOWED_NOTIFIERS as string[]).includes(notifierName)) {
+    if (!ALLOWED_NOTIFIERS.includes(notifierName)) {
       throw new RevereError(`notifier not allowed: ${notifierName}`);
     }
     const serviceIdentifier = `${capitalize(notifierName)}Notifier`;
