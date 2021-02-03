cc81968fc476e238bee5e6a7e53af441dfd034bf
Jared Johnson <jared@jaredjohnson.dev>
2021-02-03 08:06:21 -0500

    Add type annotations to bindings

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -1,19 +1,14 @@
 import * as dotenv from 'dotenv';
 import { Container } from 'inversify';
 import 'reflect-metadata';
-import { SquozeDetector } from './detectors';
+import { Detector, SquozeDetector } from './detectors';
 import { TYPES } from './inversify.types';
-import { ConsoleNotifier, DiscordNotifier } from './notifiers';
+import { ConsoleNotifier, DiscordNotifier, Notifier } from './notifiers';
 
 dotenv.config();
 
 export const container = new Container();
 
-// detectors
-container.bind(TYPES.SquozeDetector).to(SquozeDetector);
-
-// notifiers
-container.bind(TYPES.ConsoleNotifier).to(ConsoleNotifier);
-container.bind(TYPES.DiscordNotifier).to(DiscordNotifier).inSingletonScope();
-
-// subscribers
+container.bind<Detector>(TYPES.SquozeDetector).to(SquozeDetector);
+container.bind<Notifier>(TYPES.ConsoleNotifier).to(ConsoleNotifier);
+container.bind<Notifier>(TYPES.DiscordNotifier).to(DiscordNotifier);
