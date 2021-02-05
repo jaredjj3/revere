c03114a8f25987396f94c460c0633229e9cb4a70
Jared Johnson <jared@jaredjohnson.dev>
2021-02-05 09:33:12 -0500

    Create schedule service

---
+++ src/services/ScheduleService.ts
@@ -0,0 +1,29 @@
+import { Prisma, PrismaClient, Schedule } from '@prisma/client';
+
+export class ScheduleService {
+  prisma = new PrismaClient();
+
+  async findAll(): Promise<Schedule[]> {
+    return await this.prisma.schedule.findMany();
+  }
+
+  async findById(id: number): Promise<Schedule | null> {
+    return await this.prisma.schedule.findFirst({ where: { id } });
+  }
+
+  async findByName(name: string): Promise<Schedule | null> {
+    return await this.prisma.schedule.findFirst({ where: { name } });
+  }
+
+  async findActive(): Promise<Schedule[]> {
+    return await this.prisma.schedule.findMany({ where: { active: true } });
+  }
+
+  async findInactive(): Promise<Schedule[]> {
+    return await this.prisma.schedule.findMany({ where: { active: false } });
+  }
+
+  async create(args: Prisma.ScheduleCreateArgs): Promise<Schedule> {
+    return await this.prisma.schedule.create(args);
+  }
+}
