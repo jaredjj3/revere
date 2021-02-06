f99c695bd0fecf5471446fd58a084223e882766a
Jared Johnson <jared@jaredjohnson.dev>
2021-02-06 18:06:39 -0500

    Flesh out create action

--- src/commands/jobs.ts
+++ src/commands/jobs.ts
@@ -1,9 +1,23 @@
 import { Command, flags } from '@oclif/command';
 import { PrismaClient } from '@prisma/client';
+import { difference } from 'lodash';
+import * as cron from 'node-cron';
 import { NotImplementedError, RevereError } from '../errors';
 import { DEFAULT_NOTIFIERS, getNotifier, notify } from '../helpers';
 import { Notifier } from '../notifiers';
 import { CrudAction, CrudActions } from '../util';
+
+type ShowFlags = {
+  name: string;
+};
+
+type CreateFlags = {
+  name: string;
+  description?: string[];
+  command: string[];
+  cronExpression: string[];
+  active: boolean;
+};
 
 export default class Jobs extends Command {
   static description = 'describe the command here';
@@ -12,6 +26,10 @@ export default class Jobs extends Command {
     help: flags.help({ char: 'h' }),
     notifiers: flags.string({ char: 'n', multiple: true, default: DEFAULT_NOTIFIERS }),
     name: flags.string(),
+    description: flags.string({ multiple: true }),
+    command: flags.string({ multiple: true }),
+    cronExpression: flags.string({ multiple: true }),
+    active: flags.boolean({ default: true }),
   };
 
   static args = [{ name: 'operation', required: true, options: CrudActions, hidden: false }];
@@ -25,13 +43,15 @@ export default class Jobs extends Command {
 
     switch (args.operation) {
       case CrudAction.CREATE:
-        await this.create();
+        this.validate<CreateFlags>(
+          [Jobs.flags.name.name, Jobs.flags.command.name, Jobs.flags.cronExpression.name, Jobs.flags.active.name],
+          flags
+        );
+        await this.create(notifiers, flags as CreateFlags);
         break;
       case CrudAction.SHOW:
-        if (!flags.name) {
-          throw new RevereError(`missing flag: 'name'`);
-        }
-        await this.show(notifiers, flags.name);
+        this.validate<ShowFlags>([Jobs.flags.name.name], flags);
+        await this.show(notifiers, flags as ShowFlags);
         break;
       case CrudAction.UPDATE:
         await this.update();
@@ -46,12 +66,25 @@ export default class Jobs extends Command {
     this.exit(0);
   }
 
-  async create(): Promise<void> {
-    throw new NotImplementedError();
+  async create(notifiers: Notifier[], flags: CreateFlags): Promise<void> {
+    const cronExpression = flags.cronExpression.join(' ');
+    if (!cron.validate(cronExpression)) {
+      throw new RevereError(`invalid cron expression: ${flags.cronExpression}`);
+    }
+    const job = await this.prisma.job.create({
+      data: {
+        name: flags.name,
+        description: flags.description ? flags.description.join(' ') : null,
+        command: flags.command.join(' '),
+        cronExpression,
+        active: flags.active,
+      },
+    });
+    await notify(notifiers, `created job:\n${JSON.stringify(job, null, 2)}`);
   }
 
-  async show(notifiers: Notifier[], name: string): Promise<void> {
-    const job = await this.prisma.job.findFirst({ where: { name } });
+  async show(notifiers: Notifier[], flags: ShowFlags): Promise<void> {
+    const job = await this.prisma.job.findFirst({ where: { name: flags.name } });
     if (job) {
       await notify(notifiers, JSON.stringify(job, null, 2));
     } else {
@@ -66,5 +99,14 @@ export default class Jobs extends Command {
   async list(notifiers: Notifier[]): Promise<void> {
     const jobs = await this.prisma.job.findMany();
     await notify(notifiers, JSON.stringify(jobs, null, 2));
+  }
+
+  validate<T>(requiredFlagNames: string[], flags: unknown): flags is T {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const missingFlagNames = difference(requiredFlagNames, Object.keys(flags as any));
+    if (missingFlagNames.length) {
+      throw new RevereError(`missing flags: ${missingFlagNames.join(', ')}`);
+    }
+    return true;
   }
 }
