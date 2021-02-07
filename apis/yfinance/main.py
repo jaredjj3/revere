5185a285946ff293d5ffeab66674b0f84a8fbe62
Jared Johnson <jared@jaredjohnson.dev>
2021-02-06 22:23:18 -0500

    Run yfinance container

--- apis/yfinance/main.py
+++ apis/yfinance/main.py
@@ -1,6 +1,14 @@
 from flask import Flask
+import signal
+import sys
 
 app = Flask(__name__)
+
+def exit():
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, exit)
+signal.signal(signal.SIGTERM, exit)
 
 @app.route('/')
 def home():
