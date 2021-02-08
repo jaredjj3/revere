e7116f998fa692db7f1106040847559b78345cb1
Jared Johnson <jared@jaredjohnson.dev>
2021-02-07 21:42:45 -0500

    Create git util

---
+++ src/util/git.ts
@@ -0,0 +1,31 @@
+import { spawn } from 'child_process';
+import { RevereError } from '../errors';
+
+export const getCommitHash = (): Promise<string> =>
+  new Promise<string>((resolve, reject) => {
+    const git = spawn('git', ['rev-parse', 'HEAD']);
+
+    const stdout = new Array<string>();
+    const stderr = new Array<string>();
+
+    git.stdout.on('data', (chunk: Buffer) => {
+      stdout.push(chunk.toString());
+    });
+
+    git.stderr.on('data', (chunk: Buffer) => {
+      stderr.push(chunk.toString());
+    });
+
+    git.on('close', () => {
+      if (git.exitCode === 0) {
+        resolve(stdout.join(''));
+      } else {
+        reject(new RevereError(stderr.join('')));
+      }
+    });
+  });
+
+export const getGitCommitStatus = (): Promise<string> =>
+  new Promise<string>((resolve, reject) => {
+    const git = spawn('git', ['rev-parse', 'HEAD']);
+  });
