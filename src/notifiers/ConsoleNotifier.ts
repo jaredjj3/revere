68d4c7d81a612a3c0de102dbdbe0482062e963a8
Jared Johnson <jared@jaredjohnson.dev>
2021-02-09 00:16:44 -0500

    Simplify console notifier

--- src/notifiers/ConsoleNotifier.ts
+++ src/notifiers/ConsoleNotifier.ts
@@ -6,11 +6,6 @@ import { Notifier } from './types';
 @injectable()
 export class ConsoleNotifier implements Notifier {
   async notify(...messages: Message[]): Promise<void> {
-    for (const message of messages) {
-      const alert = `${message.type.toUpperCase()} ALERT`;
-      logger.info(
-        `\nSTART ${alert}=======================\n${message.timestamp}\n${message.content}\nEND ${alert}=======================\n`
-      );
-    }
+    logger.info(messages);
   }
 }
