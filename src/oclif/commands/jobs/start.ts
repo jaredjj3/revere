73e77f21996beea9b0ce71ccb3ca317278579a6a
Jared Johnson <jared@jaredjohnson.dev>
2021-02-11 23:06:58 -0500

    Nest start job under jobs topic

--- src/oclif/commands/jobs/start.ts
+++ src/oclif/commands/jobs/start.ts
@@ -1,9 +1,9 @@
 import { flags } from '@oclif/command';
-import { container } from '../../inversify.config';
-import { TYPES } from '../../inversify.constants';
-import { logger } from '../../logger';
-import { JobRunner } from '../../runners/JobRunner';
-import { LongRunningCommand } from '../LongRunningCommand';
+import { container } from '../../../inversify.config';
+import { TYPES } from '../../../inversify.constants';
+import { logger } from '../../../logger';
+import { JobRunner } from '../../../runners/JobRunner';
+import { LongRunningCommand } from '../../LongRunningCommand';
 
 export default class Start extends LongRunningCommand {
   static hidden = true;
